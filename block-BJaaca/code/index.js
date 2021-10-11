c49562c9a510b6c4ca94473539bd53c99d91efb1
Chandan Kumar <chandanchandu287@gmail.com>
2021-10-11 12:57:57 +0530

    Solve block-2 exercise of understanding objects

--- block-BJaaca/code/index.js
+++ block-BJaaca/code/index.js
@@ -2,19 +2,44 @@
 
 //  - Create a variable named `character` using `let` assign a value of empty object.
 
+let character = {
+
+}
+
+
 //  - Go to this [Arya Stark](https://awoiaf.westeros.org/index.php/Arya_Stark)
 
 //  Re-assign the value of `character` variable to and object with the key `characterName` and value of "Arya"
 
+let character = {
+    characterName: "Arya",
+}
+
+
 // - A variable named `age` is predefined with value 20. Add a new key named `character-age` with the value of `age` variable i.e 20 (don't use the value 20)
 
 let age = 20;
+let character = {
+    "character-age": age,
+}
+
 
 // - Add a new key `sur name` with the value of the surname of the character i.e "Stark". (Don't remove the space between `sur` and `name`)
 
+let character = {
+    "sur name": "Stark",
+}
+
 // - Add a new key `title` with the value title of the character i.e "Lady of Winterfell"
+let character = {
+    title: "Lady of Winterfell",
+}
 
 // - Add another property named `greet` and value should be a function when called should alert `I am [NAME HERE] and my title is [TITLE HERE]`.
+
+let character = {
+    greet: "I am character["characterName"] and my title is character["title"]",
+}
 
 // - Add a new property to the character object named `isFemale` and value will be `true`
 
@@ -39,3 +64,14 @@ let keyName = 'playedBy';
 // - Add a new key named `totalSeasons` with the value of `1 + 2 + 3 + 2`. Use this expression
 
 // - Access the value of the key `totalSeason` (it should be 8)
+
+let character = {
+    characterName: "Arya",
+    "character-age": age,
+    "sur name": "Stark",
+    title: "Lady of Winterfell",
+    greet: "I am character.characterName and my title is character.title",
+    isFemale: true,
+    isAdult ,
+    playedBy: "Maisie Williams",
+}
